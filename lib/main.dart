c7f31a46e1def4573ffd12ce3908bba61a90dfe2
Jerry Tabernero <tabernerojerry@gmail.com>
2022-07-20 21:43:47 +0800

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Stop Watch',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.black,
       ),
